82c7512b55c66945cdda7a6cedfd49b3aee25e6e
Jana-Ruth <ruthewenode73@gmail.com>
2025-07-23 23:32:16 +0100

    update Header

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,5 +1,10 @@
+import Header from "@/components/layout/Header"
+
 const Posts: React.FC = () => {
     return(
+        <div>
+            <Header/>
         <h1 className="text-center text-2xl text-bold">Posts</h1>
+        </div>
     )
 }
