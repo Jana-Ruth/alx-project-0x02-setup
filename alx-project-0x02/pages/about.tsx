f128e7384bb9da038d26a5b49e0e12a8aa7c03f6
Jana-Ruth <ruthewenode73@gmail.com>
2025-07-23 12:27:26 +0100

    update project with extra file

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header"
+
+const About: React.FC = () =>{
+    return(
+        <div>
+            <Header/>
+        <h1 className="items-center text-center">About</h1>
+        </div>
+    )
+}
+
+export default About
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header"
+
+const Home: React.FC = () =>{
+    return(
+        <div>
+    <Header/>
+        <h1 className="mt-20">Home</h1>
+        </div>
+    )
+}
+
+export default Home
