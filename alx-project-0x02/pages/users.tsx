4d750563832c55297f17ddbe179a5e5f5f0d41f6
Jana-Ruth <ruthewenode73@gmail.com>
2025-07-24 18:54:11 +0100

    userprops

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -23,7 +23,7 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export async function getStaticProps() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users: UserProps[] = await response.json();
